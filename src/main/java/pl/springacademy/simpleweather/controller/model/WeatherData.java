31ce3cf5b19727ea66c76e61f37fa9c7424a32da
bachta.pawel <bachta.pawel@gmail.com>
2022-01-29 22:13:34 +0100

    move binding data to controller model

--- src/main/java/pl/springacademy/simpleweather/controller/model/WeatherData.java
+++ src/main/java/pl/springacademy/simpleweather/controller/model/WeatherData.java
@@ -4,10 +4,13 @@ import java.time.LocalTime;
 
 import lombok.Builder;
 import lombok.Value;
+import pl.springacademy.simpleweather.service.model.OpenWeatherResponse;
 
 @Value
 @Builder
 public class WeatherData {
+
+    private final static String ICON_ENDPOINT_SUFFIX = "@2x.png";
 
     Double temp;
     Double perceivedTemp;
@@ -29,4 +32,34 @@ public class WeatherData {
 
     String errorMessage;
 
+    public static WeatherData forError(final String errorMessage) {
+        return WeatherData.builder()
+                .errorMessage(errorMessage)
+                .build();
+    }
+
+    public static WeatherData from(final OpenWeatherResponse openWeatherResponse, final String iconBaseUrl) {
+
+        return WeatherData.builder()
+                .temp(openWeatherResponse.getMain().getTemp())
+                .perceivedTemp(openWeatherResponse.getMain().getFeelsLike())
+                .tempMin(openWeatherResponse.getMain().getTempMin())
+                .tempMax(openWeatherResponse.getMain().getTempMax())
+                .humidity(openWeatherResponse.getMain().getHumidity())
+                .pressure(openWeatherResponse.getMain().getPressure())
+                .windDirection(openWeatherResponse.getWind().getDeg())
+                .windSpeed(openWeatherResponse.getWind().getSpeed())
+                .iconUrl(prepareIconUrl(openWeatherResponse.getWeather().get(0).getIcon(), iconBaseUrl))
+                //                .sunrise(openWeatherResponse.getSys().getSunrise())
+                //                .sunset(openWeatherResponse.getSys().getSunset())
+                .city(openWeatherResponse.getName())
+                .country(openWeatherResponse.getSys().getCountry())
+                .errorMessage(openWeatherResponse.getMessage())
+                .build();
+    }
+
+    private static String prepareIconUrl(final String icon, final String iconBaseUrl) {
+
+        return iconBaseUrl + icon + ICON_ENDPOINT_SUFFIX;
+    }
 }
--- src/main/java/pl/springacademy/simpleweather/service/OpenWeatherService.java
+++ src/main/java/pl/springacademy/simpleweather/service/OpenWeatherService.java
@@ -2,11 +2,9 @@ package pl.springacademy.simpleweather.service;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
@@ -14,14 +12,11 @@ import pl.springacademy.simpleweather.configuration.OpenWeatherConfiguration;
 import pl.springacademy.simpleweather.controller.model.WeatherData;
 import pl.springacademy.simpleweather.service.model.OpenWeatherResponse;
 
-import static java.util.Objects.isNull;
-
 @Service
 @RequiredArgsConstructor
 public class OpenWeatherService {
 
     private final static String BY_CITY_ENDPOINT = "?q={city}&appid={apiKey}&units=metric";
-    private final static String ICON_ENDPOINT_SUFFIX = "@2x.png";
 
     private final RestTemplate restTemplate;
     private final OpenWeatherConfiguration configuration;
@@ -42,42 +37,10 @@ public class OpenWeatherService {
                     configuration.getApiKey()
             );
         } catch (final HttpClientErrorException.NotFound e) {
-            return WeatherData.builder()
-                    .errorMessage("City " + city + " was not found")
-                    .build();
+            return WeatherData
+                    .forError("City " + city + " was not found");
         }
 
-        return bindResponseToWeatherData(response.getBody());
-    }
-
-    private WeatherData bindResponseToWeatherData(final OpenWeatherResponse response) {
-
-        if (response.getCod() != HttpStatus.OK.value()) {
-            return WeatherData.builder()
-                    .errorMessage(response.getMessage())
-                    .build();
-        }
-
-        return WeatherData.builder()
-                .temp(response.getMain().getTemp())
-                .perceivedTemp(response.getMain().getFeelsLike())
-                .tempMin(response.getMain().getTempMin())
-                .tempMax(response.getMain().getTempMax())
-                .humidity(response.getMain().getHumidity())
-                .pressure(response.getMain().getPressure())
-                .windDirection(response.getWind().getDeg())
-                .windSpeed(response.getWind().getSpeed())
-                .iconUrl(prepareIconUrl(response.getWeather().get(0).getIcon()))
-                //                .sunrise(response.getSys().getSunrise())
-                //                .sunset(response.getSys().getSunset())
-                .city(response.getName())
-                .country(response.getSys().getCountry())
-                .errorMessage(response.getMessage())
-                .build();
-    }
-
-    private String prepareIconUrl(final String icon) {
-
-        return configuration.getIconUrl() + icon + ICON_ENDPOINT_SUFFIX;
+        return WeatherData.from(response.getBody(), configuration.getIconUrl());
     }
 }
